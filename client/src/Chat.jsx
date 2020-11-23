85372f990780fb6a4d8790d5434126bde0972642
tommyrharper <thomasrobertharper@gmail.com>
2020-11-23 19:17:37 +0000

    Update Chat.jsx to use subscriptions

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -3,10 +3,11 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
+  useSubscription,
   useMutation,
   gql,
 } from "@apollo/client";
+import { WebSocketLink } from '@apollo/client/link/ws';
 import { 
   Container,
   Row,
@@ -15,13 +16,21 @@ import {
   Button
 } from 'shards-react';
 
+const link = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect: true
+  }
+});
+
 const client = new ApolloClient({
+  link,
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
 const GET_MESSAGES = gql`
-query {
+subscription {
   messages {
     id
     content
@@ -36,9 +45,7 @@ mutation ($user:String!, $content:String!) {
 }`;
 
 const Messages = ({ user }) => {
-  const { data } = useQuery(GET_MESSAGES, {
-    pollInterval: 500,
-  });
+  const { data } = useSubscription(GET_MESSAGES);
   if (!data) {
     return null;
   }
