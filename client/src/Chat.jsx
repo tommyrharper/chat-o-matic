dc83265ea69fb3c5614290dde7b8d0a444bf4369
tommyrharper <thomasrobertharper@gmail.com>
2020-11-23 18:30:43 +0000

    Update Chat.jsx to connect to server

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,14 +1,57 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import { 
+  Container,
+} from 'shards-react';
 
 const client = new ApolloClient({
-  uri: 'https://localhost:4000',
-  cache: new InMemoryCache()
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
 });
+
+const GET_MESSAGES = gql`
+query {
+  messages {
+    id
+    content
+    user
+  }
+}
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+
+  return JSON.stringify(data);
+  // return (
+  //   <>
+  //   {data.messages.map(({ id, user, content }) => (
+  //     <div
+  //       style={{
+  //         display: 'flex',
+  //         justifyContent: user === messageUser ? 'flex-end': 'flex-start',
+  //         paddingBottom = "1em"
+  //       }}
+  //     ></div>
+  //   ))}
+  //   </>
+  // )
+}
 
 const Chat = () => {
   return (
-    <div>I'm a chat window</div>
+    <Container>
+      <Messages user="Jack"/>
+    </Container>
   )
 }
 
